5ad41a62fe94c511ff4a1ed4c2459c306ee45fed
Merryfurr <merryfurr@merryfurr.ml>
2016-05-15 02:46:51 -0500

    Attempt at cursor

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -15,4 +15,17 @@ void kernel_main(void)
 	printf("Hello, kernel World!\n");
         printf("This is just a simple test of the kernal!\n");
         printf("If you can see this, you have built and ran ninesnow!\n");
+        
+}
+
+void update_cursor(int row, int col)
+{
+        unsigned short position=(row*80) + col;
+ 
+        // cursor LOW port to vga INDEX register
+        outb(0x3D4, 0x0F);
+        outb(0x3D5, (unsigned char)(position&0xFF));
+        // cursor HIGH port to vga INDEX register
+        outb(0x3D4, 0x0E);
+        outb(0x3D5, (unsigned char )((position>>8)&0xFF));
 }
